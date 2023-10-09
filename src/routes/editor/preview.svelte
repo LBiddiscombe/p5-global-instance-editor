393cb9d7bbde8bd648b4249712048484cc020f18
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2023-10-09 21:27:42 +0100

    added restart button

--- src/routes/editor/preview.svelte
+++ src/routes/editor/preview.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import P5, { type Sketch } from 'p5-svelte';
 	import p5 from 'p5';
-	import { PlayCircle, StopCircle } from 'lucide-svelte';
+	import { PlayCircle, StopCircle, RotateCcw } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 	import { instance, inputError } from '$lib/stores/codeStore';
 
 	export let output: string;
-	let sketch: Sketch;
+	let sketch: Sketch | undefined;
 	let isPlaying = true;
 	let p5Instance: P5 | undefined;
 	let p5Ref: HTMLDivElement | undefined;
+	let sketchKey = Date.now();
 
 	const handleError = (error: ErrorEvent) => {
 		error.preventDefault();
@@ -23,6 +24,7 @@
 			} catch (error) {
 				if (error instanceof Error) {
 					inputError.set(error.message);
+					sketch = undefined;
 				}
 			}
 		}
@@ -50,6 +52,16 @@
 		}
 	}
 
+	function restartSketch() {
+		if (p5Ref && p5Instance) {
+			p5Instance.remove();
+			p5Instance = undefined;
+			p5Ref.remove();
+			p5Ref = undefined;
+		}
+		sketchKey = Date.now();
+	}
+
 	$: console.log(`p5 Version: ${p5.prototype.VERSION}`);
 
 	function gotRef(e: CustomEvent) {
@@ -64,31 +76,40 @@
 
 		const { width: maxWidth, height: maxHeight } = container.getBoundingClientRect();
 
-		p5Instance.width = Math.min(maxWidth, p5Instance.width);
-		p5Instance.height = Math.min(maxHeight, p5Instance.height);
-		p5Instance._start();
+		p5Instance.resizeCanvas(
+			Math.min(maxWidth, p5Instance.width),
+			Math.min(maxHeight, p5Instance.height)
+		);
 	}
 </script>
 
 <div class="h-full flex flex-col relative">
-	<button
-		on:click={togglePlaying}
-		class="absolute max-w-fit rounded-full bg-yellow-400 hover:bg-yellow-300 hover:shadow-[4px_4px_#282825] transition-all m-2 shadow-[2px_2px_#282825] border border-[#282825]"
-	>
-		{#if isPlaying}
-			<StopCircle size={48} strokeWidth={1} absoluteStrokeWidth={true} />
-		{:else}
-			<PlayCircle size={48} strokeWidth={1} absoluteStrokeWidth={true} />
-		{/if}
-	</button>
+	<div class="absolute flex">
+		<button
+			on:click={togglePlaying}
+			class="max-w-fit rounded-full bg-yellow-400 hover:bg-yellow-300 hover:shadow-[4px_4px_#282825] transition-all m-2 shadow-[2px_2px_#282825] border border-[#282825]"
+		>
+			{#if isPlaying}
+				<StopCircle size={48} strokeWidth={1} absoluteStrokeWidth={true} />
+			{:else}
+				<PlayCircle size={48} strokeWidth={1} absoluteStrokeWidth={true} />
+			{/if}
+		</button>
+		<button
+			on:click={restartSketch}
+			class="w-12 flex justify-center items-center aspect-square rounded-full bg-yellow-400 hover:bg-yellow-300 hover:shadow-[4px_4px_#282825] transition-all m-2 shadow-[2px_2px_#282825] border border-[#282825]"
+		>
+			<RotateCcw size={32} strokeWidth={1} absoluteStrokeWidth={true} />
+		</button>
+	</div>
 	{#if sketch}
-		{#key sketch}
+		{#key sketchKey}
 			{#if isPlaying}
 				<div class="py-2 flex justify-center items-center flex-grow overflow-hidden">
 					<P5
 						{sketch}
 						parentDivStyle="border: 1px solid #000"
-						debug={false}
+						debug={true}
 						on:ref={gotRef}
 						on:instance={gotInstance}
 					/>
