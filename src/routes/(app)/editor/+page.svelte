d234f07706e13e7fbfb3f377e2d47fc65aabe515
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2023-10-18 18:05:44 +0100

    added first pass at a project pane

--- src/routes/(app)/editor/+page.svelte
+++ src/routes/(app)/editor/+page.svelte
@@ -5,6 +5,7 @@
 	import Preview from './preview.svelte';
 	import { outputStore } from '$lib/stores/codeStore';
 	import Warnings from './warnings.svelte';
+	import Project from './project.svelte';
 
 	export let data;
 
@@ -13,10 +14,7 @@
 
 <div class="grid flex-grow gap-4 overflow-auto lg:grid-cols-[auto_1fr_1fr]">
 	<div class="flex flex-col overflow-auto md:pt-3">
-		<p class="py-2">Project Files</p>
-		<div class="h-80 flex-grow rounded-2xl border border-base-content bg-base-200 p-2">
-			<p class="text-center text-sm">placeholder</p>
-		</div>
+		<Project />
 		<Examples examples={data.examples} />
 	</div>
 	<CodeEditor />
---
+++ src/routes/(app)/editor/project.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+	import { FileEdit, FileX2, FilePlus2, MoreHorizontal } from 'lucide-svelte';
+	let project = {
+		name: 'My Project',
+		files: [
+			{ name: 'sketch.js', content: 'console.log("Hello, world!")' },
+			{ name: 'class.js', content: '' }
+		],
+		editorIndex: 0,
+		persist: true
+	};
+
+	let newName = '';
+
+	function renameable(node: HTMLElement, idx: number) {
+		function handleKeyDown(event: KeyboardEvent) {
+			if (event.key === 'Enter') {
+				event.preventDefault();
+				project.files[idx].name = node.textContent ?? '';
+				menuElement?.removeAttribute('open');
+			}
+			if (event.key === 'Escape') {
+				event.preventDefault();
+				node.textContent = project.files[idx].name;
+				menuElement?.removeAttribute('open');
+			}
+		}
+
+		const menuElement = node.closest('details');
+		node.contentEditable = 'true';
+		document.addEventListener('keydown', handleKeyDown);
+
+		return {
+			destroy() {
+				document.removeEventListener('keydown', handleKeyDown);
+			}
+		};
+	}
+
+	function handleClickFile(idx: number) {
+		project.editorIndex = idx;
+	}
+
+	function addFile() {
+		const newFile = { name: newName || 'new-file.js', content: '' };
+		project.files = [...project.files, newFile];
+		project.editorIndex = project.files.length - 1;
+		newName = '';
+	}
+
+	function deleteFile(idx: number) {
+		if (project.editorIndex !== null) {
+			project.files.splice(idx, 1);
+			project.editorIndex = -1;
+		}
+	}
+
+	function handleNewFileKeyDown(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			addFile();
+		}
+		if (event.key === 'Escape') {
+			event.preventDefault();
+			newName = '';
+		}
+	}
+</script>
+
+<p class="py-2">Project Files</p>
+<div class="flex h-80 flex-grow flex-col rounded-2xl border border-base-content bg-base-200 p-2">
+	<form on:submit|preventDefault={addFile} class="flex items-center gap-2 p-1">
+		<input
+			type="text"
+			bind:value={newName}
+			on:keydown={handleNewFileKeyDown}
+			placeholder="New file name"
+			class="placeholder: w-full rounded-lg border border-base-content px-2 py-1 text-sm"
+		/>
+		<button type="submit">
+			<FilePlus2 strokeWidth={1} />
+		</button>
+	</form>
+	<ul class="flex flex-col gap-1">
+		{#each project.files as file, idx}
+			<li class="group relative flex gap-1">
+				<button
+					class="block w-full px-2 py-1 text-left"
+					class:font-medium={idx == project.editorIndex}
+					class:bg-white={idx == project.editorIndex}
+					on:click={() => handleClickFile(idx)}
+				>
+					{file.name}
+				</button>
+				<div class="group absolute right-0 top-0">
+					<details class="dropdown dropdown-end invisible mb-32 group-hover:visible">
+						<summary class="p-1 hover:bg-gray-100"><MoreHorizontal strokeWidth={1} /></summary>
+						<ul
+							class="menu dropdown-content rounded-box z-[1] w-52 border bg-base-100 py-2 shadow-md"
+						>
+							<li>
+								<div class="inlineblock !pl-1">
+									<FileEdit strokeWidth={1} /><span
+										use:renameable={idx}
+										class="cursor-text border bg-white p-1">{file.name}</span
+									>
+								</div>
+							</li>
+							<li>
+								<button class="!pl-1" on:click={() => deleteFile(idx)}>
+									<FileX2 strokeWidth={1} /> Delete {file.name}
+								</button>
+							</li>
+						</ul>
+					</details>
+				</div>
+			</li>
+		{/each}
+	</ul>
+	<div class="flex-grow"></div>
+	<p class="w-64 rounded-lg bg-yellow-300 p-2 text-center text-sm">
+		Under Construction - Not hooked up to editor, try examples for now
+	</p>
+</div>
